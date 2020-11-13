85e4c7e5270c7aa5be9ae65c92256948fe8f84ce
reshav sharna <reshav.sharma@hostbooks.com>
2020-11-13 13:02:46 +0530

    task completed for dynamic form in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Test';
+export class AppComponent implements OnInit {
+  addressList:Array<FormsModel>;
+   countryList=['USA',"India",'China',"Brazil","Kabul","Pakistan","Span","Argentina","Australia","Russia"];
+  constructor(){}
+  ngOnInit()
+  {
+    this.addressList=new Array<FormsModel>();
+    this.createForm();
+  
+  }
+  createForm()
+  {
+    let inititalForm=new FormsModel();
+    this.addressList.push(inititalForm);
+  }
+  rremoveForm(index)
+  {
+   this.addressList.splice(index,1);
+  }
+  saveForm()
+{
+  console.log("form data",this.addressList);
+}
+}
+
+
+
+export class FormsModel
+{
+  address:string;
+  street:string;
+  city:string;
+  country:string;
 }
